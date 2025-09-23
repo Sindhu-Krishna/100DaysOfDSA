fabc2497e6d3c9a887d26913ee8e62a75e8f6dc9
sindhu-krishna <17sindhukrishna@gmail.com>
2025-09-23 23:12:35 +0530

    Day 22: Rotate Image (Matrix Rotation)

---
+++ src/RotateImage.java
@@ -0,0 +1,34 @@
+public class RotateImage {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // Transpose
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] mat = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        new RotateImage().rotate(mat);
+
+        for (int[] row : mat) {
+            for (int val : row)
+                System.out.print(val + " ");
+            System.out.println();
+        }
+    }
+}
