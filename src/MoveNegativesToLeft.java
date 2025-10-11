c4c4b5ccaaa72226f57529e1e7faadc0a2834d51
sindhu-krishna <17sindhukrishna@gmail.com>
2025-10-11 22:45:52 +0530

    Day 27: Move Negatives to Left using two-pointer approach

---
+++ src/MoveNegativesToLeft.java
@@ -0,0 +1,25 @@
+public class MoveNegativesToLeft {
+    public static void rearrange(int[] arr) {
+        int left = 0, right = arr.length - 1;
+        while (left <= right) {
+            if (arr[left] < 0)
+                left++;
+            else if (arr[right] >= 0)
+                right--;
+            else {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { -1, 2, -3, 4, -5, 6 };
+        rearrange(arr);
+        for (int x : arr)
+            System.out.print(x + " ");
+    }
+}
